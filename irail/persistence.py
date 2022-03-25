fc430f59a276e462cf22e48d0de2b08fd4e54e90
ppalex <prietopantoja.alex@gmail.com>
2022-03-25 13:47:49 +0100

    add persistence file with save data function

---
+++ irail/persistence.py
@@ -0,0 +1,17 @@
+import json
+import os
+from pathlib import Path
+
+RESULT_DIRECTORY = Path(__file__).parent.parent / "data"
+
+
+def save_data(result_dict):
+
+    from_station = result_dict["from_station"]
+    to_station = result_dict["to_station"]
+
+    with open(
+        os.path.join(
+            RESULT_DIRECTORY,
+            f"to_{from_station}_from_{to_station}.json"), "w") as outfile:
+        json.dump(result_dict, outfile)
