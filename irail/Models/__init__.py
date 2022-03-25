e87ba4a838967fdf6c3b0455fb98a9c2408ef7bd
ppalex <prietopantoja.alex@gmail.com>
2022-03-25 08:19:51 +0100

    add object to __all__

--- irail/Models/__init__.py
+++ irail/Models/__init__.py
@@ -1,3 +1,7 @@
 from .manager import ConnectionsAPIManager
+from models.cleaner import ConnectionsCleaner
+from models.payload import ConnectionsPayload
+from models.api import ConnectionsApi
 
-__all__ = ["ConnectionsAPIManager"]
+__all__ = ["ConnectionsAPIManager", "ConnectionsCleaner",
+           "ConnectionsPayload", "ConnectionsApi"]
