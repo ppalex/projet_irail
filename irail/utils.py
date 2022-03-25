3d5b5d099221feff1ed304180e6ae42555cee8de
ppalex <prietopantoja.alex@gmail.com>
2022-03-25 17:23:48 +0100

    add 3 functions

---
+++ irail/utils.py
@@ -0,0 +1,10 @@
+def get_percent_next_hour_train_running(manager):
+    pass
+
+
+def get_mean_next_hour_train_delay(manager):
+    pass
+
+
+def get_number_past_2_hour_train_canceled(manager):
+    pass
