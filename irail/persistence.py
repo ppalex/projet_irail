667366541946b1782cdde60a54b68d3e95c3876f
ppalex <prietopantoja.alex@gmail.com>
2022-03-26 09:41:57 +0100

    update save data function

--- irail/persistence.py
+++ irail/persistence.py
@@ -5,19 +5,13 @@ from pathlib import Path
 RESULT_DIRECTORY = Path(__file__).parent.parent / "data"
 
 
-def save_data(from_station, to_station, next_hour_running,
-              next_hour_delay, past_2_hours_canceled):
+def save_data(data_dict):
 
-    result_dict = {
-        "from_station": from_station,
-        "to_station": to_station,
-        "next_hour_running": next_hour_running,
-        "next_hour_delay": next_hour_delay,
-        "past_2_hours_canceled": past_2_hours_canceled
-    }
+    from_station = data_dict['from_station']
+    to_station = data_dict['to_station']
 
     with open(
         os.path.join(
             RESULT_DIRECTORY,
             f"from_{from_station}_to_{to_station}.json"), "w") as outfile:
-        json.dump(result_dict, outfile, indent=4)
+        json.dump(data_dict, outfile, indent=4)
