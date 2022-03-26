3c68bf65d6737b02011b3feed347dcc624934b2c
ppalex <prietopantoja.alex@gmail.com>
2022-03-26 09:42:26 +0100

    add all functionnalities to retrieve results

--- irail/utils.py
+++ irail/utils.py
@@ -1,10 +1,39 @@
+from datetime import datetime, timedelta
+import pytz
+
+
 def get_percent_next_hour_train_running(manager):
-    pass
+    return manager.next_hour_train_running(manager.data)
 
 
 def get_mean_next_hour_train_delay(manager):
-    pass
+    return manager.next_hour_train_delay(manager.data)
 
 
 def get_number_past_2_hour_train_canceled(manager):
-    pass
+    return manager.get_total_connections_canceled(manager.data)
+
+
+def create_results(from_station,
+                   to_station,
+                   next_hour_running,
+                   next_hour_train_delay,
+                   past_2_hours_canceled):
+
+    dict = {
+        "from_station": from_station,
+        "to_station": to_station,
+        "next_hour_running": next_hour_running,
+        "next_hour_train_delay": next_hour_train_delay,
+        "past_2_hours_canceled": past_2_hours_canceled
+    }
+
+    return dict
+
+
+def get_past_2_hour_time():
+    time_now = datetime.now(tz=pytz.timezone('Europe/Brussels'))
+    time_now_min_2 = time_now - timedelta(hours=2)
+    time_now_min_2_formatted = time_now_min_2.strftime("%H%M")
+
+    return time_now_min_2_formatted
