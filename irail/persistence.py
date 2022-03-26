4af94ddcfd4946e92e48f4407da8fd709f71cab7
ppalex <prietopantoja.alex@gmail.com>
2022-03-26 09:32:14 +0100

    correct filename in save_data

--- irail/persistence.py
+++ irail/persistence.py
@@ -5,13 +5,19 @@ from pathlib import Path
 RESULT_DIRECTORY = Path(__file__).parent.parent / "data"
 
 
-def save_data(result_dict):
+def save_data(from_station, to_station, next_hour_running,
+              next_hour_delay, past_2_hours_canceled):
 
-    from_station = result_dict["from_station"]
-    to_station = result_dict["to_station"]
+    result_dict = {
+        "from_station": from_station,
+        "to_station": to_station,
+        "next_hour_running": next_hour_running,
+        "next_hour_delay": next_hour_delay,
+        "past_2_hours_canceled": past_2_hours_canceled
+    }
 
     with open(
         os.path.join(
             RESULT_DIRECTORY,
-            f"to_{from_station}_from_{to_station}.json"), "w") as outfile:
+            f"from_{from_station}_to_{to_station}.json"), "w") as outfile:
         json.dump(result_dict, outfile, indent=4)
