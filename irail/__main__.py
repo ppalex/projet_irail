d8198e28a767ddfc2c74fcd05da3543ac7820e9a
ppalex <prietopantoja.alex@gmail.com>
2022-03-26 09:42:57 +0100

    add logic in main and run

--- irail/__main__.py
+++ irail/__main__.py
@@ -1,17 +1,51 @@
+import configuration.config as config
 from models import ConnectionsAPIManager
+from persistence import save_data
+from utils import (create_results, get_mean_next_hour_train_delay,
+                   get_number_past_2_hour_train_canceled, get_past_2_hour_time,
+                   get_percent_next_hour_train_running)
+
+config.load('irail/configuration/config.yaml')
 
 
-def run():
+def run(from_station, to_station):
     """
     """
 
-    manager = ConnectionsAPIManager()
-    manager.download_connections_data("Nivelles", "Charleroi")
+    connection_manager = ConnectionsAPIManager()
 
-    [print(c) for c in manager.data]
+    connection_manager.download_connections_data(from_station, to_station)
+
+    next_hour_running = get_percent_next_hour_train_running(connection_manager)
+
+    next_hour_train_delay = get_mean_next_hour_train_delay(connection_manager)
+
+    connection_manager.download_connections_data(
+        from_station, to_station, time=get_past_2_hour_time())
+
+    past_2_hours_canceled = get_number_past_2_hour_train_canceled(
+        connection_manager)
+
+    results = create_results(
+        from_station,
+        to_station,
+        next_hour_running,
+        next_hour_train_delay,
+        past_2_hours_canceled
+
+    )
+
+    save_data(results)
 
 
 if __name__ == "__main__":
     """ Main function
     """
-    run()
+    connections = config.value['CONNECTIONS']
+
+    for line in connections.values():
+
+        station_1, station_2 = line[0], line[1]
+
+        run(station_1, station_2)
+        run(station_2, station_1)
